7fea0d6e48f18fcfea18d7f48c5d675e9d2d9ee6
Joey01010 <812841587@qq.com>
2022-12-16 13:05:19 +0800

    2022-12-7第五次提交

--- src/test/java/com/plane/sincer/SincerApplicationTests.java
+++ src/test/java/com/plane/sincer/SincerApplicationTests.java
@@ -1,6 +1,5 @@
 package com.plane.sincer;
 
-import com.plane.sincer.entity.Dept;
 import com.plane.sincer.entity.ProductList;
 import com.plane.sincer.entity.Role;
 import com.plane.sincer.entity.User;
@@ -12,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Date;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
@@ -50,28 +49,32 @@ class SincerApplicationTests {
 	void importProductList(){
 		productListService.importProductListByLocal("BOM.xlsx");
 	}
+	@Test
+	void getDept(){
+		System.out.println(deptService.getById(34));
+	}
+
+	@Test
+	void getDeptList(){
+		deptService.list().forEach(System.out::println );
+	}
+
+	@Test
+	void addRole(){
+		Role role = new Role();
+		role.setRoleName("管理员");
+		role.setPerAddr(Arrays.asList("*").toString());
+		roleService.save(role);
+	}
 
 	@Test
 	void addUser(){
-		User user = new User();
-		user.setUserName("phoebe");
-		user.setPassWord("123456");
-		user.setCreateTime(new Date());
-		Dept dept = new Dept();
-		dept.setDeptName("生产部");
-		deptService.save(dept);
-		user.setDeptId(dept.getId());
-		Role role = new Role();
-		role.setName("机修人员");
-		roleService.save(role);
-		user.setRoleId(role.getId());
-		userService.save(user);
+
 	}
 
 	@Test
 	void getUser(){
-		User user = userService.getUserByName("admin");
-		System.out.println(user);
+
 	}
 
 	@Test
